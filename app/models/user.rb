62f814f4cbc379c30e63a332690603878d57101f
climb719 <mary.oldweiler@gmail.com>
2021-05-27 10:08:02 -0400

    add relationships to models

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,6 @@
 class User < ApplicationRecord
+    has_many :recipes
+    has_many :comments
+    has_many :recipe_comments, through: :comments, source: :recipe 
+    has_many :categories, through: :recipes 
 end
--- db/migrate/20210527134150_create_users.rb
+++ db/migrate/20210527134150_create_users.rb
@@ -4,7 +4,7 @@ class CreateUsers < ActiveRecord::Migration[6.1]
       t.string :name
       t.string :email
       t.string :password_digest
-
+      has_many :recipes
       t.timestamps
     end
   end
