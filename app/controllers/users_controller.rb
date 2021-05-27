d46f8ce21f7758e9716457caa3993d4c09556848
climb719 <mary.oldweiler@gmail.com>
2021-05-27 12:50:43 -0400

    home, new, and show view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,28 @@
 class UsersController < ApplicationController
+
+    def new
+        @user = User.new
+    end 
+
+    def create
+        @user = User.new(user_params)
+        if @user.save
+            #log them in 
+            session[:user_id] = @user.id 
+            #redirect to show page
+            redirect_to user_path(@user)
+        else
+            render :new
+        end
+    end
+
+    def show
+        @user = User.find_by_id(params[:id]) #returns nil if doesn't find, .find returns throws an error if doesn't find
+    end
+
+    private
+
+    def user_params
+        params.require(:user).permit(:name, :email, :password)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,15 @@
 Rails.application.routes.draw do
+  root "sessions#home"
+  get '/signup' => 'users#new'
+  post '/signup' => 'users#create'
+  
+  #login route
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+
+  #logout route
+  delete '/logout' => 'sessions#destroy'
+ 
   resources :ingredients
   resources :categories
   resources :comments
