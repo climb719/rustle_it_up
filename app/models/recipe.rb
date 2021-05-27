2302b8773ec13bd15515b3dfc4f785c8c5be70e3
climb719 <mary.oldweiler@gmail.com>
2021-05-27 10:36:54 -0400

    update models

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -2,5 +2,5 @@ class Recipe < ApplicationRecord
   belongs_to :user
   belongs_to :category
   has_many :ingredients
-  has_many :users, through: comments
+  has_many :users, through: :comments
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,5 @@ class User < ApplicationRecord
     has_many :comments
     has_many :recipe_comments, through: :comments, source: :recipe 
     has_many :categories, through: :recipes 
+    has_secure_password #authenticate #validate passowrd & confirmation 
 end
